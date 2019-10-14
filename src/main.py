f750ef20a61ee17617fd7b1cc488805b8730446e
Natália Rodrigues <nataliamrq@gmail.com>
2019-10-14 11:35:39 -0300

    Finishing decodification.

--- src/main.py
+++ src/main.py
@@ -8,17 +8,16 @@ class HeapNode:
 
 class Heap:
     def __init__(self):
-        self.nodes = [HeapNode('0', 0)]
-        self.limit = 0
+        self.nodes = [HeapNode(None, 0)]
 
     def root(self):
-        if(self.limit > 0):
+        if(len(self.nodes) > 1):
             return self.nodes[1]
         else:
             return None
 
     def father(self, i):
-        return round(i/2)
+        return i//2
     
     def left(self, i):
         return 2 * i
@@ -32,18 +31,18 @@ class Heap:
         self.nodes[j] = aux
     
     def heapify(self, i):
-        if(self.limit <= 1):
+        if(len(self.nodes) <= 2):
             return
 
         l = self.left(i)
         r = self.right(i)
 
-        if(l <= self.limit and self.nodes[l].value < self.nodes[i].value):
+        if(l < len(self.nodes) and self.nodes[l].value < self.nodes[i].value):
             smaller = l
         else:
             smaller = i
 
-        if(r <= self.limit and self.nodes[r].value < self.nodes[smaller].value):
+        if(r < len(self.nodes) and self.nodes[r].value < self.nodes[smaller].value):
             smaller = r
 
         if(smaller != i):
@@ -60,17 +59,15 @@ class Heap:
 
     def add(self, node):
         self.nodes.append(node)
-        self.limit += 1
-        self.shift_up(self.limit)
+        self.shift_up(len(self.nodes) - 1)
 
     def remove(self):
         node = None
-        if(self.limit > 0):
-            if(self.limit > 1):
-                self.swap(1, self.limit)
+        if(len(self.nodes) > 0):
+            if(len(self.nodes) > 1):
+                self.swap(1, len(self.nodes) - 1)
             
             node = self.nodes.pop()
-            self.limit -= 1
             self.heapify(1)
 
         return node
@@ -78,15 +75,15 @@ class Heap:
 
 class HuffmanCoding:
 
-    def __init__(self, word):
-        self.word = word
+    def __init__(self, text):
+        self.text = text
         self.frequency = {}
         self.heap = Heap()
         self.codes = {}
-        self.reverse_codes = {}
+        self.encoded_text = ""
     
     def count_frequency(self):
-        for char in self.word:
+        for char in self.text:
             if not char in self.frequency:
                 self.frequency[char] = 0
             self.frequency[char] += 1
@@ -97,7 +94,7 @@ class HuffmanCoding:
             self.heap.add(node)
 
     def merge_nodes(self):
-        while(self.heap.limit > 1):
+        while(len(self.heap.nodes) > 2):
             node1 = self.heap.remove()
             node2 = self.heap.remove()
 
@@ -107,32 +104,53 @@ class HuffmanCoding:
 
             self.heap.add(merged)
 
-    def set_codes_util(self, node, code):
+    def set_codes(self, node, code):
         if(node is None):
             return
         
         if(node.key is not None):
             self.codes[node.key] = code
-            self.reverse_codes[code] = node.key
             return
 
-        self.set_codes_util(node.left, code + "0")
-        self.set_codes_util(node.right, code + "1")
+        self.set_codes(node.left, code + "0")
+        self.set_codes(node.right, code + "1")
 
-    def set_codes(self):
-        self.set_codes_util(self.heap.root(), "")
-
-    def test(self):
+    def encode(self):
         self.count_frequency()
         self.build_heap()
         self.merge_nodes()
-        self.set_codes()
+        self.set_codes(self.heap.root(), "")
 
-        # for i in range(self.heap.limit + 1):
-        #     print(self.heap.nodes[i].key + str(self.heap.nodes[i].value))
+        for char in self.text:
+            self.encoded_text += self.codes[char]
+
+    def decode(self):
+        node = self.heap.root()
+
+        for char in self.encoded_text:
+            if(char == '0'):
+                node = node.left
+            elif(char == '1'):
+                node = node.right
+            
+            if(node.key is not None):
+                print(node.key)
+                node = self.heap.root()
+
+
+    # def print_heap(self):
+    #     key = "dad"
+    #     for node in self.heap.nodes:
+    #         if(node.key is not None):
+    #             key = node.key
+    #         print(key + "-" + str(node.value))
 
 
 if __name__ == "__main__":
     test = HuffmanCoding("teste")
-    test.test()
+    test.encode()
+
+    print(test.encoded_text)
+
+    test.decode()
     
